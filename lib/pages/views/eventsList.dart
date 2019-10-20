ea5f7fe5e9a14749379edf5b96d54279861d28bf
Beatriz Da Silva Lamano <beatriz.lamano@enext.local>
2019-10-20 19:01:26 -0300

    listagem de eventos

--- lib/pages/views/eventsList.dart
+++ lib/pages/views/eventsList.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:dio/dio.dart';
+import './common/CustomShapeClipper.dart';
 
 class EventsList extends StatefulWidget {
   @override 
@@ -8,55 +9,79 @@ class EventsList extends StatefulWidget {
 }
 
 class _EventsListState extends State<EventsList> {
-  Widget _appBarTitle = new Text('Search...');
-  Icon _searchIcon = new Icon(Icons.search);
+  // Widget _appBarTitle = new Text('Search...');
+  // Icon _searchIcon = new Icon(Icons.search);
 
   @override 
   Widget build(BuildContext context) {
     return Scaffold(
-    body: ListView(
+      body: ListView(
+        // shrinkWrap: true,
         scrollDirection: Axis.vertical,
         children: <Widget>[
           Padding(
             padding: const EdgeInsets.all(20.0),
             child: Container(
-              child: new FittedBox(
+              // child: new FittedBox(
+               
                 child: Material(
                     color: Colors.white,
                     elevation: 8.0,
                     borderRadius: BorderRadius.circular(6.0),
                     shadowColor: Color(0x802196F3),
                     child: Row(
+                      
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
                         Container(
                           child: Padding(
                             padding: const EdgeInsets.fromLTRB(19.0, 6.0, 19.0, 6.0),
                             child: myDetailsContainer1(),
+                            
                           ),
                         ),
-                        FlatButton(
-                          color: Colors.redAccent,
-                          textColor: Colors.white,
-                          disabledColor: Colors.grey,
-                          disabledTextColor: Colors.black,
-                          padding: EdgeInsets.all(10.0),
+                        Container(
+                       margin: const EdgeInsets.only(top: 0.0),
+                       child: FractionallySizedBox(
+                         widthFactor: 0.6,
+                           child: FlatButton(
+                             color: Color(0xFF00A0BE),
+                             textColor: Color(0xFF525252),
+                              child: Text('SIGN UP',
+                                style: TextStyle(
+                                    fontFamily: 'Lato',
+                                    fontSize: 12.0,
+                                    color: Color(0xFF525252),
+                                    letterSpacing: 2.0
+                                )
+                              )
+                           )
+                          )
+                        )
+                        // FlatButton(
+                        
+                        //   color: Colors.redAccent,
+                        //   textColor: Colors.white,
+                        //   disabledColor: Colors.grey,
+                        //   disabledTextColor: Colors.black,
+                        //   padding: EdgeInsets.all(10.0),
                           
-                          splashColor: Colors.redAccent,
-                          onPressed: () {
-                          /*...*/
-                          },
-                          child: Text(
+                        //   splashColor: Colors.redAccent,
+                        //   onPressed: () {
+                        //   /*...*/
+                        //   },
+                        //   child: Text(
                           
-                          "Inscrever-se",
-                          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w800,    
+                        //   "Inscrever-se",
+                        //   style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w800,    
                           
-                          fontFamily: 'Raleway',) ,
-                          ),
-                        ),
-                      ],)
+                        //   fontFamily: 'Raleway',) ,
+                        //   ),
+                        // ),
+                      ],
+                    )
                 ),
-              ),
+              // ),
             ),
           ),
           
@@ -114,132 +139,85 @@ class _EventsListState extends State<EventsList> {
     );
   }
 
-  Widget myDetailsContainer2() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  // Widget _buildBar(BuildContext context) {
+  //   return new AppBar(
+  //     centerTitle: true,
+  //     title: _appBarTitle,
+  //     leading: new IconButton(
+  //       icon: _searchIcon,
+  //       onPressed: _searchPressed,
+
+  //     ),
+  //   );
+  // }
+
+  // void _searchPressed() {
+  //   setState(() {
+  //     if (this._searchIcon.icon == Icons.search) {
+  //       this._searchIcon = new Icon(Icons.close);
+  //       this._appBarTitle = new TextField(
+  //         decoration: new InputDecoration(
+  //             prefixIcon: new Icon(Icons.search),
+  //             hintText: 'Search...'
+  //         ),
+  //       );
+  //     } else {
+  //       this._searchIcon = new Icon(Icons.search);
+  //       this._appBarTitle = new Text('Search Example');
+  //     }
+  //   });
+  // }
+}
+
+class HomeScreenTopPart extends StatefulWidget {
+  @override
+  _HomeScreenTopPartState createState() => _HomeScreenTopPartState();
+}
+
+class _HomeScreenTopPartState extends State<HomeScreenTopPart> {
+  String welcomeMessage = "Fatec app";
+  
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
       children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: Container(child: Text("Chocolate Haven",
-            style: TextStyle(color: Color(0xffe6020a), fontSize: 24.0,fontWeight: FontWeight.bold),)),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: Container(child: Row(children: <Widget>[
-            Container(child: Text("4.3",
-              style: TextStyle(color: Colors.black54, fontSize: 18.0,),)),
-            Container(child: Icon(
-              FontAwesomeIcons.solidStar, color: Colors.amber, size: 15.0,),),
-            Container(child: Icon(
-              FontAwesomeIcons.solidStar, color: Colors.amber, size: 15.0,),),
-            Container(child: Icon(
-              FontAwesomeIcons.solidStar, color: Colors.amber, size: 15.0,),),
-            Container(child: Icon(
-              FontAwesomeIcons.solidStar, color: Colors.amber, size: 15.0,),),
-            Container(child: Icon(
-              FontAwesomeIcons.solidStarHalf, color: Colors.amber,
-              size: 15.0,),),
-            Container(child: Text("(75) \u00B7 1.2 mi",
-              style: TextStyle(color: Colors.black54, fontSize: 18.0,),)),
-          ],)),
-        ),
-        Container(child: Text("Pastries \u00B7 Phoenix,AZ",
-          style: TextStyle(color: Colors.black54, fontSize: 18.0,),)),
-      ],
-    );
-  }
-
-  Widget myDetailsContainer3() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: Container(child: Text("Cake Walk",
-            style: TextStyle(color: Color(0xffe6020a), fontSize: 24.0,fontWeight: FontWeight.bold),)),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: Container(child: Row(children: <Widget>[
-            Container(child: Text("4.0",
-              style: TextStyle(color: Colors.black54, fontSize: 18.0,),)),
-            Container(child: Icon(
-              FontAwesomeIcons.solidStar, color: Colors.amber, size: 15.0,),),
-            Container(child: Icon(
-              FontAwesomeIcons.solidStar, color: Colors.amber, size: 15.0,),),
-            Container(child: Icon(
-              FontAwesomeIcons.solidStar, color: Colors.amber, size: 15.0,),),
-            Container(child: Icon(
-              FontAwesomeIcons.solidStar, color: Colors.amber, size: 15.0,),),
-            Container(child: Text("(100) \u00B7 2.0 mi",
-              style: TextStyle(color: Colors.black54, fontSize: 18.0,),)),
-          ],)),
-        ),
-        Container(child: Text("Pastries \u00B7 Phoenix,AZ",
-          style: TextStyle(color: Colors.black54, fontSize: 18.0,),)),
-      ],
-    );
-  }
-
-  Widget myDetailsContainer4() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: Container(child: Text("Chocolate Bar",
-            style: TextStyle(color: Color(0xffe6020a), fontSize: 24.0,fontWeight: FontWeight.bold),)),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: Container(child: Row(children: <Widget>[
-            Container(child: Text("3.5", style: TextStyle(
-              color: Colors.black54, fontSize: 18.0,),)),
-            Container(child: Icon(
-              FontAwesomeIcons.solidStar, color: Colors.amber, size: 15.0,),),
-            Container(child: Icon(
-              FontAwesomeIcons.solidStar, color: Colors.amber, size: 15.0,),),
-            Container(child: Icon(
-              FontAwesomeIcons.solidStar, color: Colors.amber, size: 15.0,),),
-            Container(child: Icon(
-              FontAwesomeIcons.solidStarHalf, color: Colors.amber,
-              size: 15.0,),),
-            Container(child: Text("(50) \u00B7 2.5 mi",
-              style: TextStyle(color: Colors.black54, fontSize: 18.0,),)),
-          ],)),
-        ),
-        Container(child: Text("Pastries \u00B7 Phoenix,AZ",
-          style: TextStyle(color: Colors.black54, fontSize: 18.0,),)),
-      ],
-    );
-  }
-
-  Widget _buildBar(BuildContext context) {
-    return new AppBar(
-      centerTitle: true,
-      title: _appBarTitle,
-      leading: new IconButton(
-        icon: _searchIcon,
-        onPressed: _searchPressed,
-
-      ),
-    );
-  }
-
-  void _searchPressed() {
-    setState(() {
-      if (this._searchIcon.icon == Icons.search) {
-        this._searchIcon = new Icon(Icons.close);
-        this._appBarTitle = new TextField(
-          decoration: new InputDecoration(
-              prefixIcon: new Icon(Icons.search),
-              hintText: 'Search...'
+        ClipPath(
+          clipper: CustomShapeClipper(),
+          child: Container(height: 150.0, color: Colors.red, 
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 40.0,),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(60.0, 5.0, 60.0, 10.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(welcomeMessage,
+                      style: TextStyle(fontWeight: FontWeight.w800,    
+                        fontSize: 24.0,
+                        color: Colors.white,
+                        fontFamily: 'Raleway',
+                        ),
+                    ),
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Hero(
+                        tag: "hero",
+                        child: Container(
+                          padding: EdgeInsets.only(top: 50.0),
+                          height: 80.0,
+                          width: 80.0,
+                          // child: logo,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
-        );
-      } else {
-        this._searchIcon = new Icon(Icons.search);
-        this._appBarTitle = new Text('Search Example');
-      }
-    });
-  }
+        ),
+      )
+    ],
+  );
+}
 }
