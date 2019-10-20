e09f159b8ec7fdeb1ff2a69862af2a0f7bcbeb19
MatheusAMF <theus.saopaulo12@gmail.com>
2019-10-20 20:27:00 -0300

    register fix

---
+++ lib/pages/views/common/test
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulWidget {
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+class Course{
+  int id;
+  String name;
+
+  Course(this.id,this.name);
+
+  static List<Course> getList(){
+    return<Course>[Course(1,'ADSMA1'),Course(2,'ADSMA2'),Course(4,'ADSMA3'),Course(4,'ADSMA4'),Course(4,'ADSMA5'),Course(4,'ADSMA6'),Course(4,'ADSVA1'),Course(4,'ADSVA2'),Course(4,'ADSVA3'),Course(4,'ADSVA4'),Course(4,'ADSVA5'),Course(4,'ADSVA6'),
+                  Course(4,'SEGMA1'),Course(4,'SEGMA2'),Course(4,'SEGMA3'),Course(4,'SEGMA4'),Course(4,'SEGMA5'),Course(4,'SEGMA6'),Course(4,'SEGNA1'),Course(4,'SEGNA2'),Course(4,'SEGNA3'),Course(4,'SEGNA4'),Course(4,'SEGNA5'),Course(4,'SEGNA6'),
+                  Course(4,'COMEX1'),Course(4,'COMEX2'),Course(4,'COMEX3'),Course(4,'COMEX4'),Course(4,'COMEX5'),Course(4,'COMEX6'),Course(4,'JOGNA1'),Course(4,'JOGNA2'),Course(4,'JOGNA3'),Course(4,'JOGNA4'),Course(4,'JOGNA5'),Course(4,'JOGNA6')] ;
+  }
+
+}
+
+class _SignupPageState extends State<SignupPage> {
+
+  List<Course> _CourseList = Course.getList();
+  List<DropdownMenuItem<Course>> _dropdownMenuItems;
+  Course _selectedCourse;
+  @override
+  void initState(){
+    _dropdownMenuItems = buildDropdownMenuItems(_CourseList);
+    _selectedCourse = _dropdownMenuItems[0].value;
+  }
+
+  List<DropdownMenuItem<Course>> buildDropdownMenuItems(List courseList){
+    List<DropdownMenuItem<Course>> items = List();
+    for (Course course in courseList){
+      items.add(DropdownMenuItem(
+        value: course,
+        child: Text(course.name),
+      ),
+      );
+    }
+    return items;
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        resizeToAvoidBottomPadding: false,
+        body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
+            Widget>[
+          Container(
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                  child: Text(
+                    'Signup',
+                    style:
+                        TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(260.0, 125.0, 0.0, 0.0),
+                  child: Text(
+                    '.',
+                    style: TextStyle(
+                        fontSize: 80.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+              padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(
+                        labelText: 'Nome Completo',
+                        labelStyle: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey),
+                        // hintText: 'Nome Completo',
+                        // hintStyle: ,
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.green))),
+                  ),
+                  SizedBox(height: 10.0),
+                  TextField(
+                    decoration: InputDecoration(
+                        labelText: 'Email ',
+                        labelStyle: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.green))),
+                  ),
+                  SizedBox(height: 10.0),
+                  TextField(
+                    decoration: InputDecoration(
+                        labelText: 'Senha',
+                        labelStyle: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.green))),
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 10.0),
+                  TextField(
+                    decoration: InputDecoration(
+                        labelText: 'RA',
+                        labelStyle: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.green))),
+                  ),
+                  new Container(
+                    child: Center(
+                    child:Column(
+                      children:<Widget>[
+                        Text("Selecione Sua Turma"),
+                        SizedBox(height:20.0,),
+                        DropdownButton(
+                          value: _selectedCourse,
+                          items: _dropdownMenuItems, onChanged: (Course value) {},
+                        ),
+                      ]
+                    )
+                  )
+                  ),
+                  SizedBox(height: 50.0),
+                  Container(
+                      height: 40.0,
+                      child: Material(
+                        borderRadius: BorderRadius.circular(20.0),
+                        shadowColor: Colors.greenAccent,
+                        color: Colors.green,
+                        elevation: 7.0,
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: Center(
+                            child: Text(
+                              'Cadastrar',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Montserrat'),
+                            ),
+                          ),
+                        ),
+                      )),
+                //       child: InkWell(  
+                // ],
+              // )),
+        ]))]));
+  }
+}
--- lib/pages/views/register.dart
+++ lib/pages/views/register.dart
@@ -1,3 +1,4 @@
+import 'package:fatapp/pages/controllers/studentController.dart';
 import 'package:fatapp/pages/controllers/userController.dart';
 import 'package:fatapp/pages/models/user.dart';
 import 'package:fatapp/pages/views/home.dart';
@@ -11,13 +12,20 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+
+  static const courses = ["ADS", "COMEX", "SEG",'JOG'];
+  var _course;
+
   bool visibilityRA = false;
-  String _email, _password, _name, _cpf, _ra;
-  void _changed(bool visibility) {
-    setState(() {
-      visibilityRA = visibility;
-    });
-  }
+  bool visibilityCourse = false;
+  bool _isChecked = false;
+
+  TextEditingController _textRAController = new TextEditingController();
+  TextEditingController _textEmailController = new TextEditingController();
+  TextEditingController _textPasswordController = new TextEditingController();
+  TextEditingController _textNameController = new TextEditingController();
+  TextEditingController _textCPFController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -28,22 +36,22 @@ class _SignupPageState extends State<SignupPage> {
             child: Stack(
               children: <Widget>[
                 Container(
-                  padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                  padding: EdgeInsets.fromLTRB(15.0, 80.0, 0.0, 0.0),
                   child: Text(
                     'Cadastro',
                     style:
-                        TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold),
+                      TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
                   ),
                 )
               ],
             ),
           ),
           Container(
-              padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+              padding: EdgeInsets.only(top: 15.0, left: 20.0, right: 20.0),
               child: Column(
                 children: <Widget>[
                   TextField(
-                    onChanged: (input) => _name = input,
+                    controller: _textNameController,
                     decoration: InputDecoration(
                         labelText: 'Nome Completo',
                         labelStyle: TextStyle(
@@ -55,7 +63,7 @@ class _SignupPageState extends State<SignupPage> {
                   ),
                   SizedBox(height: 10.0),
                   TextField(
-                    onChanged: (input) => _email = input,
+                    controller: _textEmailController,
                     decoration: InputDecoration(
                         labelText: 'Email',
                         labelStyle: TextStyle(
@@ -67,7 +75,7 @@ class _SignupPageState extends State<SignupPage> {
                   ),
                   SizedBox(height: 10.0),
                   TextField(
-                    onChanged: (input) => _password = input,
+                    controller: _textPasswordController,
                     decoration: InputDecoration(
                         labelText: 'Senha',
                         labelStyle: TextStyle(
@@ -80,9 +88,9 @@ class _SignupPageState extends State<SignupPage> {
                   ),
                   SizedBox(height: 10.0),
                   MaskedTextField(
-                    onChange: (input) => _cpf = input,
+                    maskedTextFieldController: _textCPFController,
                     mask: "xxx.xxx.xxx-xx",
-                    maxLength: 12,
+                    maxLength: 14,
                     keyboardType: TextInputType.number,
                     inputDecoration: new InputDecoration(
                         labelText: 'CPF',
@@ -93,33 +101,69 @@ class _SignupPageState extends State<SignupPage> {
                         focusedBorder: UnderlineInputBorder(
                             borderSide: BorderSide(color: Colors.green))),
                   ),
-                  MaskedTextField(
-                    onChange: (input) => _ra = input,
-                    maxLength: 12,
-                    keyboardType: TextInputType.number,
-                    inputDecoration: new InputDecoration(
+                  CheckboxListTile(
+                    title: Text("Você é um estudante da FATEC?"),
+                    value: _isChecked,
+                    onChanged: (val) {
+                      setState(() {
+                      _isChecked = val;
+                      if (val == true) {
+                        visibilityRA = true;
+                        visibilityCourse = true;
+                      } else {
+                        visibilityRA = false;
+                        visibilityCourse = false;
+                      }
+                   });
+                  }),
+                  Visibility(visible: visibilityCourse, child:DropdownButton(
+                    value: _course,
+                    hint: Text("Escolha seu curso"),
+                    onChanged: (newValue) {
+                      setState(() {
+                        _course = newValue;
+                      });
+                    },
+                    items: courses.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  )),
+                  Visibility(
+                    visible: visibilityRA, child: MaskedTextField(
+                      maskedTextFieldController: _textRAController,
+                      maxLength: 12,
+                      keyboardType: TextInputType.number,
+                      inputDecoration: new InputDecoration(
                         labelText: 'RA',
                         labelStyle: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey),
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey),
                         focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green))),
+                        borderSide: BorderSide(color: Colors.green)),
+                      )
+                    )
                   ),
-                  SizedBox(height: 50.0),
+                  SizedBox(height: 20.0),
                   Container(
                       height: 40.0,
                       child: Material(
                         borderRadius: BorderRadius.circular(20.0),
                         color: Colors.redAccent,
                         elevation: 7.0,
-                        child: GestureDetector(
-                          onTap: () {
+                        child: RaisedButton(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          onPressed: () {
                             register();
                           },
+                          color: Colors.redAccent,
                           child: Center(
-                            child: Text(
-                              'Cadastrar',
+                            child: Text('Cadastrar',
                               style: TextStyle(
                                   color: Colors.white,
                                   fontWeight: FontWeight.bold,
@@ -127,15 +171,38 @@ class _SignupPageState extends State<SignupPage> {
                             ),
                           ),
                         ),
-                      )),
+                      )
+                    ),
                 //       child: InkWell(  
                 // ],
               // )),
         ]))]));
   }
   Future<void> register() async {
+    var _name = _textNameController.text, 
+        _cpf = _textCPFController.text, 
+        _password = _textPasswordController.text,
+        _email = _textEmailController.text,
+        _ra = _textRAController.text;
+
     var jsonData = '{ "name" : "$_name", "cpf" : "$_cpf", "email" : "$_email", "password" : "$_password" }';
     print(jsonData);
+    if(visibilityRA) {
+      var student = '{ "ra" : "$_ra", "course" : "$_course" }';
+      try {
+        await StudentController().create(student, null);
+      } catch(e) {      
+        Fluttertoast.showToast(
+          msg: e.toString(),
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIos: 2,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0
+        );
+      }
+    }
     try {
       final response = await UserController().create(jsonData);
       User user = User.fromJson(response);
