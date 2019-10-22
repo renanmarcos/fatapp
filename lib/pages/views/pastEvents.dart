a67ec9b4865fb3536e439b6df61f2fd3c26adb14
Bia Lamano <48034278+bialamano@users.noreply.github.com>
2019-10-22 14:48:53 -0300

    listas de eventos passados e atual + inicio da barra inferior (#15)
    * listagem de eventos
    
    * listas de eventos atuais e passados

---
+++ lib/pages/views/pastEvents.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import './pastEventsYears.dart';
+
+class PastEvents extends StatelessWidget {
+  const PastEvents({Key key}) : super(key: key);
+
+  
+  @override
+  Widget build(BuildContext context) {
+    final events = ['Fatecnologia', 'ADS Day', 'Comex Day', 'Games Day'];
+
+     return ListView.builder(
+        itemCount: events.length,
+        itemBuilder: (context, index) {
+          return Card( 
+            elevation: 2.0,
+            margin: EdgeInsets.fromLTRB(15.0, 5.0, 15.0, 5.0),
+            child: InkWell(
+              splashColor: Colors.blue.withAlpha(30),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PastEventsYears()),
+                );
+              },
+            child: ListTile(
+              title: Text(events[index], 
+              style: TextStyle(color: Colors.black, fontFamily: 'Raleway',
+              fontSize: 18.0)),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
