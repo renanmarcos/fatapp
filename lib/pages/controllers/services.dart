d18a805b806fbd049c61f7260346f12de720b0ad
MatheusAMF <theus.saopaulo12@gmail.com>
2019-11-01 07:41:49 -0300

    Alguns ajustes na tela de cadastro e perfil (#26)
    * Alguns ajustes na tela de cadastro e perfil
    
    * Revisão

--- lib/pages/controllers/services.dart
+++ lib/pages/controllers/services.dart
@@ -74,7 +74,7 @@ class Services {
     return ResponseHandling().handling(response);
   }
 
-  putData(url, id, dataToPut, token, [complementation]) async {
+  putData(url, id, dataToPut, token, [complementation = ""]) async {
     var response;
     try {
       response = await http.Client().put(
--- lib/pages/views/updateUser.dart
+++ lib/pages/views/updateUser.dart
@@ -1,5 +1,7 @@
+import 'package:fatapp/pages/controllers/courseController.dart';
 import 'package:fatapp/pages/controllers/responseHandling.dart';
 import 'package:fatapp/pages/controllers/userController.dart';
+import 'package:fatapp/pages/models/course.dart';
 import 'package:fatapp/pages/models/user.dart';
 import 'package:fatapp/pages/views/changePassword.dart';
 import 'package:fatapp/pages/views/home.dart';
@@ -15,33 +17,30 @@ class UpdateUserPage extends StatefulWidget {
 }
 
 class _UpdateUserPageState extends State<UpdateUserPage> {
+  String _course;
+  List<Course> courseList;
+  bool _isChecked = false;
+
   TextEditingController _textRAController = new TextEditingController();
   TextEditingController _textEmailController = new TextEditingController();
   TextEditingController _textNameController = new TextEditingController();
   TextEditingController _textCPFController = new TextEditingController();
+  void initState() {
+    this.getValues();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    this.getValues();
-    
     return new Scaffold(
+        appBar: new AppBar(
+            title: new Text('Perfil',
+                style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold))),
         resizeToAvoidBottomPadding: false,
-        body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
-            Widget>[
-          Container(
-            child: Stack(
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.fromLTRB(15.0, 80.0, 0.0, 0.0),
-                  child: Text(
-                    'Perfil',
-                    style:
-                        TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-                  ),
-                )
-              ],
-            ),
-          ),
+        body: SingleChildScrollView(
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
+                    Widget>[
           Container(
               padding: EdgeInsets.only(top: 15.0, left: 20.0, right: 20.0),
               child: Column(children: <Widget>[
@@ -56,7 +55,7 @@ class _UpdateUserPageState extends State<UpdateUserPage> {
                       focusedBorder: UnderlineInputBorder(
                           borderSide: BorderSide(color: Colors.green))),
                 ),
-                SizedBox(height: 10.0),
+                SizedBox(height: 8.0),
                 TextField(
                   controller: _textEmailController,
                   decoration: InputDecoration(
@@ -68,7 +67,7 @@ class _UpdateUserPageState extends State<UpdateUserPage> {
                       focusedBorder: UnderlineInputBorder(
                           borderSide: BorderSide(color: Colors.green))),
                 ),
-                SizedBox(height: 10.0),
+                SizedBox(height: 8.0),
                 MaskedTextField(
                   maskedTextFieldController: _textCPFController,
                   mask: "xxx.xxx.xxx-xx",
@@ -84,12 +83,44 @@ class _UpdateUserPageState extends State<UpdateUserPage> {
                       focusedBorder: UnderlineInputBorder(
                           borderSide: BorderSide(color: Colors.green))),
                 ),
+                SizedBox(height: 10.0),
+                CheckboxListTile(
+                    title: Text("Você é um estudante da FATEC?"),
+                    value: _isChecked,
+                    onChanged: (val) {
+                      setState(() {
+                        _isChecked = val;
+                      });
+                    }),
+                FutureBuilder<List<Course>>(
+                    future: CourseController().getCourses(),
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      if (!snapshot.hasData) {
+                        return Center(child: CircularProgressIndicator());
+                      } else {
+                        courseList = snapshot.data;
+                        return DropdownButton<String>(
+                          hint: Text('Escolha seu curso'),
+                          value: _course,
+                          onChanged: (String course) {
+                            setState(() {
+                              _course = course;
+                            });
+                          },
+                          items: courseList.map((Course course) {
+                            return DropdownMenuItem<String>(
+                              value: course.acronym,
+                              child: Text(course.acronym),
+                            );
+                          }).toList(),
+                        );
+                      }
+                    }),
                 MaskedTextField(
                     maskedTextFieldController: _textRAController,
                     maxLength: 13,
                     keyboardType: TextInputType.number,
                     inputDecoration: new InputDecoration(
-                      enabled: false,
                       labelText: 'RA',
                       labelStyle: TextStyle(
                           fontFamily: 'Montserrat',
@@ -98,19 +129,29 @@ class _UpdateUserPageState extends State<UpdateUserPage> {
                       focusedBorder: UnderlineInputBorder(
                           borderSide: BorderSide(color: Colors.green)),
                     )),
-                SizedBox(height: 20.0),
+                SizedBox(height: 10.0),
                 Container(
                     height: 40.0,
                     child: Material(
                       borderRadius: BorderRadius.circular(20.0),
                       color: Colors.redAccent,
-                      child: Center(
-                        child: Text(
-                          'Atualizar',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: 'Montserrat'),
+                      elevation: 7.0,
+                      child: RaisedButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        onPressed: () {
+                          this.update();
+                        },
+                        color: Colors.redAccent,
+                        child: Center(
+                          child: Text(
+                            'Atualizar',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Montserrat'),
+                          ),
                         ),
                       ),
                     )),
@@ -144,11 +185,8 @@ class _UpdateUserPageState extends State<UpdateUserPage> {
                         ),
                       ),
                     )),
-                //       child: InkWell(
-                // ],
-                // )),
               ]))
-        ]));
+        ])));
   }
 
   getValues() {
@@ -157,20 +195,30 @@ class _UpdateUserPageState extends State<UpdateUserPage> {
     _textEmailController.text = widget.user.email;
     if (widget.user.student != null) {
       _textRAController.text = widget.user.student.ra;
+      _isChecked = true;
+      //_course = widget.user.student.course.acronym;
     }
   }
 
   Future<void> update() async {
     var _name = _textNameController.text, _email = _textEmailController.text;
-
     ResponseHandling().validateEmail(_email);
     try {
       var jsonData = '{ "name" : "$_name", "email" : "$_email" }';
       final update = await UserController()
           .update(widget.user.id, jsonData, widget.user.token);
       User user = User.fromJson(update, widget.user.token);
-      Navigator.push(context,
+      Fluttertoast.showToast(
+          msg: "Cadastro atualizado com sucesso",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIos: 2,
+          backgroundColor: Colors.green,
+          textColor: Colors.white,
+          fontSize: 16.0);
+      Navigator.pushReplacement(context,
           MaterialPageRoute(builder: (context) => HomePage(user: user)));
+      //Falta ajustar estudante
     } catch (e) {
       Fluttertoast.showToast(
           msg: e.toString(),
