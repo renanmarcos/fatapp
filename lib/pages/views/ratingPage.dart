82c1e119d71e289afd3386de7dbf1eaf33c29fef
MatheusAMF <theus.saopaulo12@gmail.com>
2019-11-30 08:48:32 -0300

    Tela de avaliação com botão (#46)
    * ta dando ruim mas vai dar bom
    
    * corrigindo bugs
    
    * fixes
    
    * Mudanças
    
    * create button

--- lib/pages/views/ratingPage.dart
+++ lib/pages/views/ratingPage.dart
@@ -2,6 +2,7 @@ import 'package:fatapp/pages/models/user.dart';
 import 'package:fatapp/pages/views/home.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:gradient_widgets/gradient_widgets.dart';
 import 'package:smooth_star_rating/smooth_star_rating.dart';
 import 'package:fatapp/pages/controllers/activityController.dart';
 import 'package:fatapp/pages/models/acitivity.dart';
@@ -28,56 +29,86 @@ class _RatingStarState extends State<RatingStar> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Rating bar demo',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
+    return Scaffold(
           appBar: AppBar(
           backgroundColor: const Color(0xFFCE0000),
           title: Text('Avaliação'),
           elevation: 0,
         ),
-        body: Center(
-          child: new ListView(
-            shrinkWrap: true,
-            
-            padding: const EdgeInsets.all(50.0),
-            
-            children: <Widget>[
-            Text('Dê uma nota para o evento: ' + widget.activity.title,
-              style: TextStyle(fontSize: 24.0),
-              textAlign: TextAlign.center,
-             ),
-             SizedBox(height: 45.0),
-             Center(
-              child: 
-                 SmoothStarRating(
-                  rating: rating,
-                  size: 45,
-                  starCount: 5,
-                  spacing: 2.0,
-                  borderColor: Colors.redAccent,
-                  color: Colors.yellow,
-                  onRatingChanged: (value) {
-                  setState(() {
-                    rating = value;
-                    this.rateActivity(rating, widget.user.id, widget.user.token);
-                  });
-                },
-             )),
-            ]
-          )
-        )
-      ),
+        resizeToAvoidBottomInset: true,
+        body: SingleChildScrollView(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+              Container(
+                  padding: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+                  child: Column(children: <Widget>[
+                  new ListView(
+                    shrinkWrap: true,
+                    padding: const EdgeInsets.all(50.0),
+                    children: <Widget>[
+                    Text('Dê uma nota para o evento: ' + widget.activity.title,
+                      style: TextStyle(fontSize: 24.0),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 45.0),
+                    Center(
+                      child: 
+                        SmoothStarRating(
+                          rating: rating,
+                          size: 45,
+                          starCount: 5,
+                          spacing: 2.0,
+                          borderColor: Colors.redAccent,
+                          color: Colors.yellow,
+                          onRatingChanged: (value) {
+                          setState(() {
+                            this.rating = value;
+                          });
+                        },
+                    )),
+                    SizedBox(height: 50.0),
+                    Container(
+                      height: 50.0,
+                        child: Material(
+                          borderRadius: BorderRadius.circular(20.0),
+                          child: GradientButton(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            increaseWidthBy: 50.0,
+                            increaseHeightBy: 10.0,
+                            gradient: Gradients.buildGradient(
+                              Alignment.topLeft, Alignment.bottomRight, [
+                              const Color(0xFFCE0000),
+                              const Color(0xFFB71C1C)
+                            ]),
+                            callback: () async {
+                              this.rateActivity(this.rating, widget.user.id, widget.user.token);
+                            },
+                              child: Center(
+                                child: Text(
+                                  'Cadastre-se',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'Montserrat'),
+                                ),
+                              ),
+                            ),
+                          )),
+                  ]
+                )]
+              )
+            )
+        ])
+      )
     );
   }
 
-  void rateActivity(ratingValue, userId, token) {
+  Future<void> rateActivity(ratingValue, userId, token) async {
     var jsonRate = '{ "userId" : "$userId", "numberOfStars" : "$ratingValue"}';
-    ActivityController().rateActivity(widget.activity.id, jsonRate, token);
+    await ActivityController().rateActivity(widget.activity.id, jsonRate, token);
     Navigator.pushReplacement(context,
       MaterialPageRoute(builder: (context) => HomePage(user: widget.user))); 
     Fluttertoast.showToast(
