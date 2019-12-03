0e35405a72727db20980327d2c1bc80fbe349e28
Renan Marcos Ferreira <renan.ferreira18@fatec.sp.gov.br>
2019-12-03 00:00:53 -0300

    Fix location

--- lib/pages/views/maps.dart
+++ lib/pages/views/maps.dart
@@ -6,9 +6,14 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 class MapsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Localização',
-      home: Map(),
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text("Localização"),
+        backgroundColor: const Color(0xFFCE0000),
+        elevation: 0,
+      ),
+      resizeToAvoidBottomInset: true,
+      body: Map(),
     );
   }
 }
@@ -20,7 +25,7 @@ class Map extends StatefulWidget {
 
 class MapState extends State<Map> {
   Completer<GoogleMapController> _controller = Completer();
-  static final position = LatLng(-23.6377382, -46.581072117);
+  static final position = LatLng(-23.637119, -46.578780);
   static final CameraPosition _myLocation =
       CameraPosition(target: position, zoom: 15);
   final Set<Marker> _markers = new Set();
@@ -36,6 +41,7 @@ class MapState extends State<Map> {
 
     return new Scaffold(
       body: GoogleMap(
+          mapToolbarEnabled: true,
           zoomGesturesEnabled: true,
           markers: _markers,
           initialCameraPosition: _myLocation,
